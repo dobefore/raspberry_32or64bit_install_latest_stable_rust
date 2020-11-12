c45465948ef2014fefaa1974de0f4948fbbbacc4
sampuly <1432338032@qq.com>
2020-11-12 10:22:20 +0800

    update

--- install.py
+++ install.py
@@ -3,6 +3,6 @@ import os
 print('download and install snap...')
 os.system('sudo apt install snap')
 print('install rustup...')
-os.system('snap install rustup --classic')
+os.system('sudo snap install rustup --classic')
 print('install latest stable rust...')
 os.system('rustup toolchain install stable')
